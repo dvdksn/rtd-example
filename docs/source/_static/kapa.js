f3ca8870d38bb48192d85b69057a050b1c2c903a
David Karlsson <35727626+dvdksn@users.noreply.github.com>
2025-03-06 14:09:18 +0100

    move widget up
    Signed-off-by: David Karlsson <35727626+dvdksn@users.noreply.github.com>

--- docs/source/_static/kapa.js
+++ docs/source/_static/kapa.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   script.setAttribute("data-website-id", "9c402dc4-ac1c-418c-b5d7-9d0a5ddc4e9c");
   script.setAttribute("data-project-name", "kapa.ai");
   script.setAttribute("data-project-color", "#000");
+  script.setAttribute("data-button-position-bottom", "120px");
   script.async = true;
   document.head.appendChild(script);
 });
